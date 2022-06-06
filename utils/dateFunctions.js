5e5ddd849fbb4b5bcd17e17fd594d27642b2797d
erick <uriel.noriegam@gmail.com>
2022-06-06 10:32:57 -0600

    Hot fix aaaa

--- utils/dateFunctions.js
+++ utils/dateFunctions.js
@@ -311,8 +311,8 @@ export const getBeginEndMonth = (prevMonth = false, prevYear = false) => {
 
 export const currentDate = () => {
  const day = new Date().getDate();
- const month = new Date.getMonth() + 1;
- const year = new Date.getFullYear();
+ const month = new Date().getMonth() + 1;
+ const year = new Date().getFullYear();
 
  return{
    day,
