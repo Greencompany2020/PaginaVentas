0077a4abb719e6dcb370526e05793afb8c960455
JoshAlexis <joshyelc@gmail.com>
2022-08-18 16:16:33 -0500

    refactor: Variables de entorno y gitignore

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const webpack = require('webpack')
 
 module.exports = {
   reactStrictMode: true,
-  // webpack: config => {
-  //   config.resolve.modules.push(path.resolve('./'));
-  //   config.plugins.push(
-  //     new webpack.DefinePlugin({
-  //       'process.env.NEXT_PUBLIC_API_URL': JSON.stringify(process.env.NEXT_PUBLIC_API_URL)
-  //     })
-  //   )
-  //   return config;
-  // },
   experimental: {
     outputStandalone: true,
   },
