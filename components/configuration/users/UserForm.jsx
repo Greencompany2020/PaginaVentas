1d9e4c7e88e8d5f844977d4cede9d29e7f8fcd31
Erick <uriel.noriegam@gmail.com>
2022-08-25 13:14:33 -0600

    Hot fix -Se ageo cerrar el formulario al boton de cancelar

--- components/configuration/users/UserForm.jsx
+++ components/configuration/users/UserForm.jsx
@@ -76,7 +76,7 @@ export default function UserForm({item, groups, addNewUser, updateUser, handleTo
           </div>
         </div>
         <div className="flex flex-row justify-end space-x-2 mt-6">
-          <input type='reset' value='Cancelar' className="secondary-btn w-28"/>
+          <input type='reset' value='Cancelar' className="secondary-btn w-28" onClick={handleToggle}/>
           <input type='submit' value='Guardar' className="primary-btn w-28"/>
         </div>
       </Form>
