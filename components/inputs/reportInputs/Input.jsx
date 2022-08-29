6fd4d95ba310d44a9d458338e4c20691b4851ccd
Erick <uriel.noriegam@gmail.com>
2022-08-29 11:16:37 -0600

    Cambios en los inputs de fecha -Se agrego una etiqueta al campo de fechas

--- components/inputs/reportInputs/Input.jsx
+++ components/inputs/reportInputs/Input.jsx
@@ -5,22 +5,15 @@ import { isSafari } from 'react-device-detect';
 export default function Input(props) {
     const {label,...rest} = props;
     const [field, meta] = useField(props);
-
-    const styledForNotSafari = 'w-full h-8 border rounded-md pl-2 border-slate-400 disabled:bg-gray-100 disabled:border-gray-100 outline-none';
-    const styledForSafari = 'w-12 bg-gray-300 border border-slate-400 rounded h-8 ';
-
-    const styledForDivNotSafari = '';
-    const styledForDivSafari = 'flex items-center space-x-2';
-
     const isDateAndSafari = (type) => (isSafari && type === 'date');
 
 
     return (
         <label className={`w-full flex flex-col text-sm font-semibold text-gray-600 ${rest?.flex}`} htmlFor={props?.id || props?.name}>
              <span>{label}</span>
-             <div className={isDateAndSafari(props.type) ? styledForDivSafari : styledForDivNotSafari}>
-                <input  {...rest} {...field}  className={isDateAndSafari(rest.type) ? styledForSafari : styledForNotSafari}/>
-                {isDateAndSafari() && <span className='font-bold'>{meta.value}</span>}
+             <div className={'relative flex items-center space-x-2'}>
+                <input  {...rest} {...field}  className={'w-full h-8 border rounded-md pl-2 border-slate-400 disabled:bg-gray-100 disabled:border-gray-100 outline-none'}/>
+                {isDateAndSafari(props.type) && <span className='absolute right-4'>{meta.value}</span>}
             </div>
             {(meta.touched && meta.error) &&  <span className='font-semibold text-red-500 text-right'>{meta.error}</span>}
         </label>
