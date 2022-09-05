04fac579dd48dcea1727d91e905a87811e12c763
Erick <uriel.noriegam@gmail.com>
2022-09-05 16:17:09 -0600

    hot fix Inicio y fin de semana -Se modifico la funcion de semanales

--- components/inputs/InputOfTheDate.jsx
+++ components/inputs/InputOfTheDate.jsx
@@ -5,7 +5,7 @@ const InputDate = ({ value, onChange }) => {
     <label htmlFor="fecha" className='flex flex-col text-sm'>
       <span className='font-semibold'>De la fecha</span>
       <div className={'relative flex items-center space-x-2'}>
-        <input type="date" name="fecha" id="" placeholder={value} className={'w-full h-8 border rounded-md pl-2 border-slate-400 disabled:bg-gray-100 disabled:border-gray-100 outline-none'} value={value} onChange={onChange} />
+        <input type="date" name="fecha" id=""  className={'w-full h-8 border rounded-md pl-2 border-slate-400 disabled:bg-gray-100 disabled:border-gray-100 outline-none'} value={value} onChange={onChange} />
         {(isSafari && !isMobile)&& <span className="absolute right-4">{value}</span>}
       </div>
     </label>
